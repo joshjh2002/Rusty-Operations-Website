0ec5f35628fa4d379939f34ff43c7bcb6ab6ddef
joshjh2002 <joshhelman@hotmail.co.uk>
2023-07-03 19:34:02 +0100

    Removed redundant code

--- src/app/components/NavbarItem.js
+++ src/app/components/NavbarItem.js
@@ -24,8 +24,6 @@ export default function Navbar() {
 
   const router = useRouter();
 
-  useEffect(() => {}, [pathname]);
-
   return (
     <section id="navbar" className="navbar">
       <nav className="bg-white shadow-lg">
