9420e20655413abef7daa1be7795854f79f15162
Josh Helman <joshhelman@hotmail.co.uk>
2023-07-17 12:25:00 +0100

    Added servers which pull from firebase

--- src/app/servers/page.js
+++ src/app/servers/page.js
@@ -1,14 +1,27 @@
 "use client";
 
 import Navbar from "../components/navbar.js";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 
-import { servers } from "../data/servers.json";
+import { db } from "../firebaseInit.js";
+import { ref, onValue } from "firebase/database";
 
 export default function Page() {
+  const [servers, setServers] = useState([]);
   useEffect(() => {
     document.title = "Rusty Operations | Servers";
+
+    const servers = ref(db, `servers`);
+    onValue(servers, (snapshot) => {
+      const data = snapshot.val();
+      console.log(data);
+      let temp = [];
+      data.forEach((item) => {
+        temp.push({ name: item.name, api: item.api });
+      });
+      setServers(temp);
+    });
   }, []);
 
   return (
@@ -20,9 +33,9 @@ export default function Page() {
           <h1>Servers</h1>
           <div className="card-container">
             {servers.map((server) => (
-              <div className="card">
+              <div key={server.name} className="card">
                 <div className="card-title">
-                  <h2>{server}</h2>
+                  <h2>{server.name}</h2>
                 </div>
                 <div className="card-image">
                   <Image
