70a27901711c82954d38ae10fa4576e14bb2c8af
Josh Helman <joshhelman@hotmail.co.uk>
2023-07-22 16:19:28 +0100

    Made socials a component

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import Footer from "./components/footer.js";
 import React, { useEffect } from "react";
 import Image from "next/image";
 import LiveData from "./components/livedata.js";
+import Socials from "./components/socials.js";
 
 export default function Home() {
   useEffect(() => {
@@ -56,62 +57,7 @@ export default function Home() {
             </div>
           </div>
           <LiveData />
-          <div className="grid grid-cols-5 mt-16 gap-8 bg-[#4449] place-items-center rounded-xl p-6 m-auto sm:w-max w-full">
-            <a href="https://twitter.com/rustyoperations" target="_blank">
-              <Image
-                src="/img/socials/twitter.png"
-                width={48}
-                height={48}
-                alt="Twitter"
-                className="filter invert"
-              />
-            </a>
-            <a href="https://www.facebook.com/RustyOperations" target="_blank">
-              <Image
-                src="/img/socials/facebook.png"
-                width={48}
-                height={48}
-                alt="Facebook"
-                className="filter invert"
-              />
-            </a>
-            <a
-              href="https://www.instagram.com/rusty_operations/"
-              target="_blank"
-            >
-              <Image
-                src="/img/socials/instagram.png"
-                width={48}
-                height={48}
-                alt="Instagram"
-                className="filter invert"
-              />
-            </a>
-            <a
-              href="https://www.youtube.com/channel/UCm-FUo4TagET2kA-JpVpwWw"
-              target="_blank"
-            >
-              <Image
-                src="/img/socials/youtube.png"
-                width={48}
-                height={48}
-                alt="Youtube"
-                className="filter invert"
-              />
-            </a>
-            <a
-              href="https://steamcommunity.com/groups/rusty-operations"
-              target="_blank"
-            >
-              <Image
-                src="/img/socials/steam.png"
-                width={48}
-                height={48}
-                alt="Steam"
-                className="filter invert"
-              />
-            </a>
-          </div>
+          <Socials />
         </section>
       </div>
       <Footer />
