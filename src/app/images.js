bb3aa9f0f5f26a230d4a7dec7574080457a63071
Josh Helman <joshhelman@hotmail.co.uk>
2023-10-04 13:39:54 +0100

    Testing halloween images

--- src/app/images.js
+++ src/app/images.js
@@ -1,10 +1,10 @@
 // Import the functions you need from the SDKs you need
 
 const Images = {
-  banner: "/img/rust-banner.jpg",
-  banner_notext: "/img/rust-banner-notext.jpg",
-  logo: "/img/rust-logo.jpg",
-  icon: "favicon.ico",
+  banner: "/img/seasonal/halloween/rust-banner.jpg",
+  banner_notext: "/img/seasonal/halloween/rust-banner-notext.jpg",
+  logo: "/img/seasonal/halloween/rust-logo.jpg",
+  icon: "/img/seasonal/halloween/favicon.ico",
 };
 
 export default Images;
