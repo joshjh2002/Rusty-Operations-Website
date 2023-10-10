1f14a3ef693b96a387ccc8fe1d1e688b772e71f1
joshjh2002 <joshhelman@hotmail.co.uk>
2023-10-10 19:31:14 +0100

    Redirect old article to new route

--- next.config.js
+++ next.config.js
@@ -6,6 +6,15 @@ const nextConfig = {
   },
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: "/news/server-event-sept-6",
+        destination: "/news/events/jefferson-asylum-event-23",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
