d60c5d30381e4c17b48eaa537745d54676f58aea
joshjh2002 <joshhelman@hotmail.co.uk>
2023-11-07 18:53:57 +0000

    Updated images

--- src/app/images.js
+++ src/app/images.js
@@ -1,10 +1,10 @@
 // Import the functions you need from the SDKs you need
 
 const Images = {
-  banner: "/img/seasonal/halloween/rust-banner.jpg",
-  banner_notext: "/img/seasonal/halloween/rust-banner-notext.jpg",
-  logo: "/img/seasonal/halloween/rust-logo.jpg",
-  icon: "/img/seasonal/halloween/favicon.ico",
+  banner: "/img/rust-banner.jpg",
+  banner_notext: "/img/rust-banner-notext.jpg",
+  logo: "/img/rust-logo.jpg",
+  icon: "/img/favicon.ico",
 };
 
 export default Images;
