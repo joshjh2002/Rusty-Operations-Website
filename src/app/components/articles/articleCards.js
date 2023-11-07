3e279560576bb080a38ffb18c664da14bd3e2312
joshjh2002 <joshhelman@hotmail.co.uk>
2023-11-07 20:37:35 +0000

    Themed styling

--- src/app/components/articles/articleCards.js
+++ src/app/components/articles/articleCards.js
@@ -55,9 +55,9 @@ export default function ArticleCards({ type }) {
   return (
     <>
       <div className="w-full">
-        <div className="relative h-10 w-full min-w-[200px]">
+        <div className="relative h-10 w-full min-w-[200px] rusty-font">
           <input
-            className="peer h-full w-full rounded-[7px] border border-white border-t-transparent bg-transparent px-3 py-2.5 !pr-9 font-sans text-sm font-normal text-white outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-white placeholder-shown:border-t-white focus:border-2 focus:border-white focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-white"
+            className="peer h-full w-full rounded-[7px] border im rusty-font border-[#ff9500] border-t-transparent bg-transparent px-3 py-2.5 !pr-9 font-sans text-sm font-normal text-white outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-[#ff9500] placeholder-shown:border-t-[#ff9500] focus:border-2 focus:border-[#ff9500] focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-white"
             placeholder=" "
             onChange={(e) => {
               if (e.target.value.trim() === "") return setSearch(articles);
@@ -70,7 +70,7 @@ export default function ArticleCards({ type }) {
               );
             }}
           />
-          <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-white transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-white before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-white after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-white peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-white peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-white peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-white peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-white">
+          <label className="before:content[' '] after:content[' '] rusty-font pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-white transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-[#ff9500] before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-[#ff9500] after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-white peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-white peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-[#ff9500] peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-[#ff9500] peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-white">
             Search for articles
           </label>
         </div>
